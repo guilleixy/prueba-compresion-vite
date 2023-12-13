1572ccc5b8c4220b061887e3477bd14f86f3082a
guille <129344574+guilleixy@users.noreply.github.com>
2023-12-13 12:04:28 +0100

    separadito en chunks

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ViteCompressionPlugin from 'vite-plugin-compression';
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+  ],
   build: {
     sourcemap: true,
     rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          reactRouter: ['react-router-dom'],
+          reactSpringThree: ['@react-spring/three'],
+          reactThreeDrei: ['@react-three/drei'],
+          reactThreeFiber: ['@react-three/fiber'],
+          emailjs: ['@emailjs/browser'],
+          reactHelmet: ['react-helmet'],
+          reactTooltip: ['react-tooltip'],
+          reactVerticalTimelineComponent: ['react-vertical-timeline-component'],
+        },
+      },
       plugins: [
         ViteCompressionPlugin({
           verbose: true,
